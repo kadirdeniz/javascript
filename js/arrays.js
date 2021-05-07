4eec7574dd181fb3f6662a3eaf596e3932a59bee
Kadir <deniz-kadir2014@hotmail.com>
2021-05-07 14:10:04 +0300

    javasript

---
+++ js/arrays.js
@@ -0,0 +1,89 @@
+const arr = [2,3,5,7,11]
+
+// JS objects are name indexes
+// JS arrays are number indexes
+
+
+// <!-- ARRAYS -->
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr["length"])
+
+
+console.log(arr)
+console.log("Deleted Value "+arr.shift())
+console.log(arr)
+arr.unshift(100)
+
+console.log("Index of 2 is : "+arr.indexOf(3))
+console.log("Index of 2 is : "+arr.lastIndexOf(11))
+console.log("Between values :"+ arr.slice(0,3))
+
+const newArr = []
+
+newArr["name"] = "Kadir"
+newArr["surname"] = "Deniz"
+console.log(newArr.name.toUpperCase())
+
+console.log(newArr)
+newArr.push("DenemeValue")
+console.log(newArr)
+console.log(newArr[0])
+newArr.pop()
+console.log(newArr)
+
+// <!-- OBJECTS -->
+
+
+const firstObject = {
+    squirrel:false,
+    events:["ekle","oku","güncelle","sil"]
+}
+console.log(firstObject.events[0])
+firstObject.kitap = "Silmarillon"
+console.log(firstObject)
+
+console.log("kitap" in firstObject) // True
+delete firstObject.kitap
+console.log(typeof delete firstObject.kitap)
+console.log("kitap" in firstObject) // False
+
+console.log(Object.keys(firstObject))
+Object.assign(firstObject,{name:"Silmarillion",Author:"Tolkien"})
+console.log(Object.keys(firstObject))
+
+// String
+const str = "Çok Önemli    Bir Metin  Yazısı     Bu"
+console.log(str)
+console.log(str.length)
+console.log(str.trim())
+console.log(str.trim().toUpperCase())
+console.log(str.trim().indexOf("ir"))
+
+
+// Exerices
+
+const range = (start,end,step=1)=>{
+    const arr = []
+    for(let i = start;i<=end;i+=step){
+        arr.push(i)
+    }
+    return arr;
+}
+const sum = (arr)=>{
+    let sum = 0
+    arr.forEach((each)=>{
+        sum+=each
+    })
+    return sum
+}
+const reverse = (arr)=>{
+    const result = []
+    for(let i = arr.length;i>0;i-=1){
+        result.push(i)
+    }
+    return result;
+}
+
+console.log(range(1,5))
+console.log(reverse(range(1,5)))
---
+++ js/chessboard.js
@@ -0,0 +1,77 @@
+// For satır 8
+// For sütun 8
+// if satır%2 = 1 ise baştan
+// elseif satır%2 = 0 bir satır atla
+
+let result="";
+const chessBoard = function(row){
+    const column = row/2
+    for(let i =1;i<=row;i++){
+        for(let j=1;j<=column;j++){
+            if(i%2==0){
+                result +="# "
+            }else if(i%2==1){
+                result+=" #"
+            }
+        }
+        result+="\n"
+    }
+    return result
+}
+//console.log(chessBoard(8))
+//DONE
+
+// <!-- Arrow Function -->
+const square = (x)=>{return x*x}
+const easySquare = x => x*x;
+
+console.log(square(4))//16
+console.log(easySquare(4))//16
+
+console.log(easySquare(4,"kadir","deniz"))//ignore other params
+
+const power = (base,exponenet = 2)=>{
+    let result = 1;
+    for (let i = 1; i<= exponenet;i++){
+        result *= base;
+    }
+    return result
+}
+
+console.log(power(3,3))// Ignore exponent param 2 and set it 3
+console.log(power(3))// set it 2 as exponenet
+
+
+// <!-- Closures -->
+
+// treated functions as value
+
+const multiplier = (factor)=>{
+    return number=>number*factor
+}
+// ilginx
+let twice = multiplier(2)
+console.log(twice(5))
+
+
+// <!-- recursion -->
+/*
+* Therefore, always start by writing something that’s correct and easy to understand.
+*  If you’re worried that it’s too slow—which it usually isn’t since most code simply isn’t
+* executed often enough to take any significant amount of time—you can measure afterward and improve it if necessary.
+* */
+
+/*
+* A pure function is a specific kind of value-producing function that not only has no side effects but also doesn’t rely on side effects from other code—for example,
+* it doesn’t read global bindings whose value might change.*/
+
+const min = (a,b)=>{
+    if(a>b){
+        return b
+    }else if(a<b){
+        return a
+    }else{
+        return 1
+    }
+}
+console.log(min(3,2))
