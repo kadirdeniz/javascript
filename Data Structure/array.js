1e48d2ecf683d21232a3b742684f6bb2a5e3ad62
Kadir <deniz-kadir2014@hotmail.com>
2021-05-12 14:14:10 +0300

    tiktokte

---
+++ Data Structure/array.js
@@ -0,0 +1,38 @@
+// Tik Tac Toe
+
+const board = [["X","X","O"],["O","X","O"],["O","O","X"]]
+
+const checkValues = (arr)=>{
+    // Check Rows isEqual
+    for(let row of arr){
+        if(checkXorO(row)){
+            return 1
+        }
+    }
+    // Check Columns
+    for(let col = 0;col<=2;col++){
+        const column = [arr[0][col],arr[1][col],arr[2][col]]
+        if(checkXorO(column)){
+            return 1
+        }
+    }
+    return 0
+}
+
+const checkXorO = (arr)=>{
+    return isAllX(arr) || isAllO(arr)
+}
+const isAllO = (arr)=>{
+    return arr[0] == "O" && arr[1] == "O" && arr[2] == "O"
+}
+const isAllX = (arr)=>{
+    return arr[0] == "X" && arr[1] == "X" && arr[2] == "X"
+}
+const isGameFinished = (arr)=>{
+    if(checkValues(arr)){
+        console.log("Game Finished")
+    }else{
+        console.log("Game Continious")
+    }
+}
+isGameFinished(board)
---
+++ Data Structure/strings.js
@@ -0,0 +1,30 @@
+const name = "Kadir"
+
+// return given index of string
+console.log(name.charAt(1))
+
+// Return between given indexes
+console.log(name.substring(1,4))
+
+// Delete given indexes and return rest of it
+console.log(name.substring(2))
+
+console.log("a">"b")
+
+console.log(name.indexOf("Ka"))
+
+const person="kadir,deniz,22"
+const splittedPerson = person.split(",")
+console.log(splittedPerson)
+
+const newPerson = person.replace("kadir","Yunu Emre")
+console.log(newPerson)
+
+// // Regular Expressions
+// ^: Indicates the start of a string/line
+// \d: Finds any digit
+// [abc]: Finds any character between the brackets
+// [^abc]: Finds any character not between the brackets
+// [0-9]: Finds any digit between the brackets
+// [^0-9]: Finds any digit not between the brackets
+// (x|y): Finds any of the alternatives specified
