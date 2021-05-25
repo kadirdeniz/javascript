1c61e6d4d2c104cc37a0d67ed3edfcd63e577181
Kadir <deniz-kadir2014@hotmail.com>
2021-05-25 21:50:50 +0300

    Trendyol Backend Bootcamp Case 1

--- Data Structure/case1.js
+++ Data Structure/case1.js
@@ -1,3 +1,10 @@
+// Write a function that, given as integer N,returns the smallest integer greater than N,
+// the sum of whose digits in twice as big as the sum of digits of N
+
+// 1- Given N = 14 that returns 19. The sum of digits is twice as big as sum of digits of 14 (1+4=5)
+// 2- Given N = 10, the function should return 11
+// 3- Given N = 99, the function should return 9999
+
 const number = 14
 
 const getNumbersSum = (num)=>{
@@ -11,7 +18,7 @@ const getNumbersSum = (num)=>{
 
 const findSmallestValue = (num)=>{
     const sumValue = getNumbersSum(num)
-    for(let x=num;x<=500;x++){
+    for(let x=num;x<=10000;x++){
         if(getNumbersSum(x)==sumValue*2){
             return x
         }
